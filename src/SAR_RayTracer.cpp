4b4f5b7417640cd7f06c040345cdf38090b36e33
bnjnrthrp <benji.northrop@gmail.com>
2024-10-27 14:05:07 -0700

    Add project files.

---
+++ src/SAR_RayTracer.cpp
@@ -0,0 +1,25 @@
+﻿// SAR_RayTracer.cpp : Defines the entry point for the application.
+//
+
+#include "../include/SAR_RayTracer.h"
+#include <iostream>
+
+int main()
+{
+	int nx, ny;
+
+	nx = 200;
+	ny = 100;
+	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2;
+			int ir = int(255.99 * r);
+			int ig = int(255.99 * g);
+			int ib = int(255.99 * b);
+			std::cout << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+}
