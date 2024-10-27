50014145745fe2a2d6e20dc602854f79fe0f52ca
bnjnrthrp <benji.northrop@gmail.com>
2024-10-27 14:38:07 -0700

    func: add vec3 class (header)

--- src/SAR_RayTracer.cpp
+++ src/SAR_RayTracer.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "../include/SAR_RayTracer.h"
+#include "../include/vec3.h"
 #include <iostream>
 
 int main()
@@ -13,12 +14,10 @@ int main()
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
-			float b = 0.2;
-			int ir = int(255.99 * r);
-			int ig = int(255.99 * g);
-			int ib = int(255.99 * b);
+			vec3 col(double(i) / double(nx), double(j) / double(ny), 0.2);
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
 			std::cout << ir << " " << ig << " " << ib << "\n";
 		}
 	}
