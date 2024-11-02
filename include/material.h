66f053676022f4f573a9369a9c5e2fb1f42c03b2
bnjnrthrp <benji.northrop@gmail.com>
2024-11-02 06:55:29 -0700

    func: Complete random scene - finish ray trace in a weekend

--- include/material.h
+++ include/material.h
@@ -67,9 +67,9 @@ public:
 	float fuzz;
 };
 
-class dialectric : public material {
+class dielectric : public material {
 public:
-	dialectric(float ri) : ref_idx(ri) {}
+	dielectric(float ri) : ref_idx(ri) {}
 	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
 		vec3 outward_normal;
 		vec3 reflected = reflect(r_in.direction(), rec.normal);
--- src/SAR_RayTracer.cpp
+++ src/SAR_RayTracer.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <cfloat>
+#include <cmath>
 #include <stdlib.h>
 #include "../include/hitable_list.h"
 #include "../include/sphere.h"
@@ -36,6 +37,48 @@ vec3 color(const ray& r, hitable *world, int depth) {
 		return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 	}
 }
+hitable* random_scene();
+hitable* random_scene() {
+	int n = 500;
+	hitable** list = new hitable * [n + 1];
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+	int i = 1;
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			float choose_mat = drand48();
+			vec3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+				if (choose_mat < 0.8) {  // diffuse
+					list[i++] = new sphere(
+						center, 0.2,
+						new lambertian(vec3(drand48() * drand48(),
+							drand48() * drand48(),
+							drand48() * drand48()))
+					);
+				}
+				else if (choose_mat < 0.95) { // metal
+					list[i++] = new sphere(
+						center, 0.2,
+						new metal(vec3(0.5 * (1 + drand48()),
+							0.5 * (1 + drand48()),
+							0.5 * (1 + drand48())),
+							0.5 * drand48())
+					);
+				}
+				else {  // glass
+					list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+				}
+			}
+		}
+	}
+
+	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+	list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+	return new hitable_list(list, i);
+}
+
 
 int main()
 {
@@ -45,16 +88,14 @@ int main()
 	ny = 100;
 	ns = 100;
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	hitable* world = random_scene();
 
-	hitable* list[5];
-	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
-	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3f));
-	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dialectric(1.5));
-	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dialectric(1.5));
-
-	hitable* world = new hitable_list(list, 5);
-	camera cam;
+	vec3 lookfrom(13, 2, 3);
+	vec3 lookat(0, 0, 0);
+	vec3 vup(0, 1, 0);
+	float dist_to_focus = 10.0;
+	float aperture = 0.1;
+	camera cam(lookfrom, lookat, vup, 20, float(nx) / float(ny), aperture, dist_to_focus);
 
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
